f9bbfb5a8ab0bb44d9f40d9ee8564a4712b224af
Arnau Giribet PyCharm <arnaugiribetmarch@gmail.com>
2023-03-01 17:29:02 +0100

    Better app

--- server.R
+++ server.R
@@ -12,51 +12,59 @@ shinyServer(function(input, output) {
   
   # Import Data and clean it
   load('01-data_download/03dadesWithROI.RData')
-  setDT(dades)
-  dades[,rentabilidad := round(rnorm(nrow(dades),8,1),2)]
-  dades[,alquiler:= round(rentabilidad*(price*1.15)/12/100)]
-  dades[,rentabilidad_col:= cut(rentabilidad,c(-Inf,6,8,9,10,Inf), labels=c('Inferior a 6','6-8','8-9','9-10','Superior a 10'))]
+  
+  dadesSale[,ROIpct:= round(ROIpct,1)]
+  dadesSale[,SuggestedRentalPrice:= round(SuggestedRentalPrice)]
+    
+  value_labels<-c(-Inf,6,8,9,10,12,Inf)
+  color_labels<-c('Inferior a 6','6-8','8-9','9-10','10-12','Superior a 12')
+  
+  dadesSale[,rentabilidad_color:= as.character(cut(ROIpct,value_labels, labels=color_labels))] #important que sigui character
+  
+  lowers<-(!dadesSale$rentabilidad_color %in% c('10-12','Superior a 12'))
+  dadesSale<-dadesSale[c(which(lowers),which(!lowers)[1:50]),]
   
   # new column for the popup label
   
-  dades <- mutate(dades, cntnt=paste0('<strong>Rentabilidad: </strong>',rentabilidad,
-                                      '<br><strong>Precio:</strong> ', price,
-                                      '<br><strong>Precio por m2:</strong> ', priceByArea,
-                                      '<br><strong>Alquiler estimado:</strong> ', alquiler,
-                                      '<br><strong>Tipo de propiedad:</strong> ',propertyType,
-                                      '<br><strong>m2:</strong> ',size,
-                                      '<br><strong>Habitaciones:</strong> ',rooms,
-                                      '<br><strong>Altura:</strong> ',floor,
-                                      '<br><strong>Ascensor:</strong> ',hasLift,
-                                      '<br><strong>Exterior:</strong> ',exterior,
-                                      '<br><strong>Estado:</strong> ',status,
-                                      '<br><strong>Dirección:</strong> ',address,
-                                      '<br><strong>Enlace:</strong> ',url))
-
-  # create a color paletter for category type in the data file
+  dadesSale <- mutate(dadesSale, cntnt=paste0('<strong>Rentabilidad: </strong>',paste(ROIpct,'%',sep=''),
+                                              '<br><strong>Precio:</strong> ', price,
+                                              '<br><strong>Precio por m2:</strong> ', priceByArea,
+                                              '<br><strong>Alquiler estimado:</strong> ', SuggestedRentalPrice,
+                                              '<br><strong>Tipo de propiedad:</strong> ',propertyType,
+                                              '<br><strong>m2:</strong> ',size,
+                                              '<br><strong>Habitaciones:</strong> ',rooms,
+                                              '<br><strong>Altura:</strong> ',floor,
+                                              '<br><strong>Ascensor:</strong> ',hasLift,
+                                              '<br><strong>Exterior:</strong> ',exterior,
+                                              '<br><strong>Estado:</strong> ',status,
+                                              '<br><strong>Dirección:</strong> ',address,
+                                              '<br><strong>Enlace:</strong> ',url))
   
-  pal <- colorFactor(pal = c('#FF625E','#FFBE65','#F5EEA5', '#D2FDBB', '#77DD76'), 
-                     domain = c('Inferior a 6', "6-8", '8-9', '9-10','Superior a 10'))
+  # create a color palette for category type in the data file
+  colors_vec<-c('#FF625E','#FFBE65','#F5EEA5', '#D2FDBB', '#77DD76' ,'#0CC078')
+  pal <- colorFactor(palette = colors_vec, 
+                     levels = color_labels)
    
   # create the leaflet map  
   output$bbmap <- renderLeaflet({
-      leaflet(dades) %>% 
+      leaflet(dadesSale) %>% 
       addCircles(lng = ~longitude, lat = ~latitude) %>% 
-      addTiles() %>%
-      addCircleMarkers(data = dades, lat =  ~latitude, lng =~longitude, 
-                       radius = 3, popup = ~as.character(cntnt), 
-                       color = ~pal(rentabilidad_col),
-                       stroke = FALSE, fillOpacity = 0.8)%>%
-      addLegend(pal=pal, values=dades$rentabilidad_col,opacity=1, na.label = "Not Available")%>%
+      addTiles(urlTemplate = "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", attribution = 'Google') %>%
+      addCircleMarkers(data=dadesSale, lat =  ~latitude, lng =~longitude, 
+                       radius = 8, popup = ~as.character(cntnt), 
+                       fillColor = ~pal(rentabilidad_color),
+                       stroke = T, color = "black", opacity=0.25, fillOpacity = 1)%>%
+      addLegend(colors=colors_vec,labels=color_labels, values= ~rentabilidad_color,opacity=1, na.label = "Not Available")%>%
       addEasyButton(easyButton(
         icon="fa-crosshairs", title="ME",
-        onClick=JS("function(btn, map){ map.locate({setView: true}); }")))
+        onClick=JS("function(btn, map){ map.locate({setView: true}); }"))) %>%
+      setView(lng = 1.747969, lat = 41.912181, zoom=8)
         })
   
   #create a data object to display data
   
   output$data <-DT::renderDataTable(datatable(
-      dades[,c('rentabilidad','price','priceByArea','alquiler','propertyType','size','rooms','floor','hasLift','exterior',
+      dadesSale[,c('ROIpct','price','priceByArea','SuggestedRentalPrice','propertyType','size','rooms','floor','hasLift','exterior',
                'status','address','url')],filter = 'top',
       colnames = c("Rentabilidad", "Precio", "Precio por m2", "Alquiler estimado", "Tipo de propiedad", "m2","Habitaciones",
                    "Altura","Ascensor","Exterior","Estado", "Dirección", "Enlace")))
--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ library(shiny)
 library(leaflet)
 
 navbarPage("Rentabilidad de pisos", id="main",
-           tabPanel("Mapa", leafletOutput("bbmap", height=1000)),
+           tabPanel("Mapa", leafletOutput("bbmap", height='600px')),
            tabPanel("Datos", DT::dataTableOutput("data")),
            tabPanel("Información",includeMarkdown("readme_shiny.md")))
 
